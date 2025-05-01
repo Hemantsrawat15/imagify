d6b4927cd6e90d5f8b07c4e2c1f7494c391be788
Enter <Enter>
2025-05-02 01:24:25 +0530

    navbar creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import Home from "./pages/Home";
 import BuyCredit from "./pages/BuyCredit";
 import Result from "./pages/Result";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/buyCredit" element={<BuyCredit />} />
+        <Route path="/buy" element={<BuyCredit />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </div>
