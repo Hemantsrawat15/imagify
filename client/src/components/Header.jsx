59310c7e4561192de19bbca3d8642a7199eca530
Enter <Enter>
2025-05-02 19:32:50 +0530

    Add motion library for animations, implement login component, and enhance header with animated elements. Update context for login state management and modify navbar for login functionality.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,53 @@
 import React from "react";
 import { assets } from "../assets/assets.js";
+import { motion } from "motion/react";
 
 function Header() {
   return (
-    <div className="flex flex-col justify-center items-center text-center my-20">
-      <div className="text-stone-500 inline-flex text-center gap-2 bg0white px-6 py-1 rounded-full border border-neutral-500">
+    <motion.div
+      className="flex flex-col justify-center items-center text-center my-20"
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+    >
+      <motion.div
+        className="text-stone-500 inline-flex text-center gap-2 bg0white px-6 py-1 rounded-full border border-neutral-500"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2, duration: 0.8 }}
+      >
         <p>Best text to image generator</p>
         <img src={assets.star_icon} alt="" />
-      </div>
-      <h1 className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center">
+      </motion.div>
+      <motion.h1
+        className="text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.4, duration: 2 }}
+      >
         Turn text to <span className="text-blue-600">image</span>,in seconds
-      </h1>
-      <p className="text-center max-w-xl mx-auto mt-5">
+      </motion.h1>
+      <motion.p
+        className="text-center max-w-xl mx-auto mt-5"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6, duration: 0.8 }}
+      >
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds — just type, and watch the magic happen.
-      </p>
+      </motion.p>
 
-      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+      <motion.button
+        className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ default: { duration: 0.5 }, opacity: { delay: 0.8 } }}
+      >
         Generate Images <img className="h-6" src={assets.star_group} alt="" />
-      </button>
+      </motion.button>
 
       <div className="flex flex-wrap justify-center mt-16 gap-3">
         {Array(6)
@@ -28,7 +57,7 @@ function Header() {
               className="rounded hover:scale-105 transition-all duraton-300 cursor-pointer max-sm:w-10"
               key={index}
               width={80}
-              src={index%2 === 0 ? assets.sample_img_1 : assets.sample_img_2}
+              src={index % 2 === 0 ? assets.sample_img_1 : assets.sample_img_2}
               alt=""
             />
           ))}
@@ -36,7 +65,7 @@ function Header() {
       <p className="text-center text-neutral-600 mt-2">
         Generated images from imagify
       </p>
-    </div>
+    </motion.div>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { AppContext } from "../context/AppContext.jsx";
 import { useContext } from "react";
 
 function Navbar() {
-  const {user} = useContext(AppContext);
+  const {user, setShowLogin} = useContext(AppContext);
 
   const navigate = useNavigate();
 
@@ -51,7 +51,7 @@ function Navbar() {
             <p onClick={() => navigate("/buy")} className="cursor-pointer">
               Pricing
             </p>
-            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
+            <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full" onClick={()=>setShowLogin(true)}>
               Login
             </button>
           </div>
